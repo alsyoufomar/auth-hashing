69027d436079d36745f89b880a2c3c0611768328
Omar Alsyouf <omaralsoyouf@gmail.com>
2022-04-12 11:40:01 +0100

    handle error, hash password & send token

--- requirements/01 - User Registration.md
+++ requirements/01 - User Registration.md
@@ -1,9 +1,9 @@
 # User Registration
 
 1. Implement the `/register` controller function in `src/router.js`.
-    1. The route must accept a `username` and `password` in the request body.
-    2. The password must be hashed.
-    3. The user must be created with the hashed password.
-    4. The route must send back the created user, including its hashed password, in the response. *(This is purely for checking your work, don't do this in the real world!)*
+   1. The route must accept a `username` and `password` in the request body.
+   2. The password must be hashed.
+   3. The user must be created with the hashed password.
+   4. The route must send back the created user, including its hashed password, in the response. _(This is purely for checking your work, don't do this in the real world!)_
 
 https://github.com/kelektiv/node.bcrypt.js#with-promises
--- src/router.js
+++ src/router.js
@@ -5,13 +5,42 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 const router = express.Router();
+const key = '87764d1a-92dc-4ced-a758-9c898c31d525'
+const saltRounds = 10;
 
 router.post('/register', async (req, res) => {
-    
+  let { username, password } = req.body
+  bcrypt.hash(password, saltRounds, async function (err, hash) {
+    password = hash
+    const createdUser = await prisma.user.create({
+      data: { username, password }
+    })
+    res.json({ data: createdUser });
+  });
+
 });
 
 router.post('/login', async (req, res) => {
-    
+  const { username, password } = req.body
+  const user = await prisma.user.findUnique({ where: { username } })
+  if (user) {
+    const match = await bcrypt.compare(password, user.password);
+    if (match) {
+      console.log('maaaatch')
+      const token = jwt.sign(username, key);
+      res.json({ token });
+    }
+    else {
+      console.log('Wrong Password!!')
+      res.status(404)
+      res.json({ error: 'Wrong Password!!' })
+    }
+  }
+  else {
+    res.status(404)
+    res.json({ error: 'User Not Found' })
+  }
 });
 
 module.exports = router;
+
